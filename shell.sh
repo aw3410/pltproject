0aedb86db894ec3d5f4d93b868e803b125244aa6
aw3410 <167843845+aw3410@users.noreply.github.com>
2024-10-17 21:49:24 -0400

    added installation instructions shell.sh

--- shell.sh
+++ shell.sh
@@ -1,2 +1,10 @@
 #!/bin/bash 
 python scanner.py
+
+In order to run a scanner like the one described above, you would need to follow the following installation steps:
+
+Installation Steps for Mac: 
+1. Install homebrew 
+2. Install bash (if needed)
+3. Install python with brew install python  
+4. Run the shell script in terminal 
