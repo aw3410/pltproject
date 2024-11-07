8b71042cd85cae469451bdc131912ea244dc57d9
Kira Ariyan <kna2121@barnard.edu>
2024-11-07 15:40:18 -0500

    created token class

--- parser.py
+++ parser.py
@@ -1,1 +1,18 @@
 
+class Token:
+    def __init__(self, token_type, value):
+        self.type = token_type 
+        self.value = value 
+
+    
+tokens = [
+    Token(type='CASTSPELL', value='castSpell'),
+    Token(type='IDENTIFIER', value='awaken'),
+    Token(type='IDENTIFIER', value='princess'),
+    Token(type='PAINT', value='paint'),
+    Token(type='IDENTIFIER', value='crown'),
+    Token(type='STRING', value='shining')
+]
+
+
+
